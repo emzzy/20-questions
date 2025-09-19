e189cce012d7976ac1f646be39123487a9c11ca1
emzzy <ezehemmanuel265@gmail.com>
2025-09-19 23:28:16 +0100

    Lottery

--- python/02-lottery.py
+++ python/02-lottery.py
@@ -1,6 +1,15 @@
 import random
 
 random_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-user_action = input("Press any key ")
-for number in range(random_numbers):
-    print(number)
+secret_num = []
+for x in range(3):
+    secret_num.append(random.randint(0, 9))
+print(secret_num)
+if 7 in secret_num:
+    print("Congratulations")
+else:
+    print("Try again! Better luck next time.")
+
+#user_action = input("Press any key ")
+
+#print(random.sample(random_numbers, 3))
