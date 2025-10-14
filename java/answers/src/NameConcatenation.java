fa3cf7a0acc87a05054c4a7353e2d08474cb1657
emzzy <ezehemmanuel265@gmail.com>
2025-10-14 23:56:19 +0100

    Name Concatenation

--- java/answers/src/NameConcatenation.java
+++ java/answers/src/NameConcatenation.java
@@ -1,3 +1,21 @@
-public class NameConcatenation {
+import java.util.Scanner;
 
+public class NameConcatenation {
+    public static void main(String[] myArgs){
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter your first name, last name, and age: ");
+
+        String firstName = userInput.nextLine();
+        String lastName = userInput.nextLine();
+        int age = userInput.nextInt();
+
+        System.out.printf("Welcome, %s %s born %d", firstName, lastName, age);
+        System.out.println("What is your gender?\nEnter Male/Female or Other: ");
+
+        String gender = userInput.nextLine();
+
+
+
+    }
 }
