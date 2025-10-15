df96ee7bddeb68f5fe0b58febc45fb2731ceb85f
emzzy <ezehemmanuel265@gmail.com>
2025-10-15 02:39:59 +0100

    Name Concatenation

--- java/answers/src/NameConcatenation.java
+++ java/answers/src/NameConcatenation.java
@@ -2,20 +2,32 @@ import java.util.Scanner;
 
 public class NameConcatenation {
     public static void main(String[] myArgs){
-        Scanner userInput = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your first name, last name, and age: ");
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
 
-        String firstName = userInput.nextLine();
-        String lastName = userInput.nextLine();
-        int age = userInput.nextInt();
+        System.out.print("Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int userAge = scanner.nextInt();
+        scanner.nextLine();
+        int age = 2025 - userAge;
 
         System.out.printf("Welcome, %s %s born %d", firstName, lastName, age);
-        System.out.println("What is your gender?\nEnter Male/Female or Other: ");
 
-        String gender = userInput.nextLine();
+        System.out.println("\nWhat is your gender?\nEnter Male/Female or Other: ");
+        String gender = scanner.nextLine();
+        String role = "";
 
+        if (gender.equalsIgnoreCase("male")){
+            role = "son";
+        } else if (gender.equalsIgnoreCase("female")) {
+            role = "daughter";
+        }
+        System.out.printf("%s, %s of %s %d", firstName, role, lastName, age);
 
-
+        scanner.close();
     }
 }
